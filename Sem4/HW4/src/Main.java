5a2a03996d429022492f2af3f018c1077c7e97f4
PurosovMV <mihail.purosov@gmail.com>
2023-12-02 18:01:04 +0300

    correct main

--- Sem4/HW4/src/Main.java
+++ Sem4/HW4/src/Main.java
@@ -2,20 +2,19 @@ import exception.*;
 public class Main {
     public static void main(String[] args) {
         try {
-            Service account = Service.create(-900);
+            Service account = Service.create(900);
             account.printBalance();
             System.out.println();
             account.makeDebit(5000);
             account.printBalance();
             System.out.println();
-            account.makeWithdrawal(8100);
+            account.makeWithdrawal(100);
             account.printBalance();
 
         } catch (CustomIllegalArgumentException ex){
             System.out.println(ex.getMessage());
         } catch (CustomInsufficientFundsException e) {
-            System.out.printf("%s Баланс = %d. Лимит = %d",
-                    e.getMessage(), e.getFirstArgument(), e.getSecondArgument());
+            System.out.printf(e.getMessage(), e.getFirstArgument(), e.getSecondArgument());
         }
 
     }
